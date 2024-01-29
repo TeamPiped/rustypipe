0a362f7129370ed2031051589f7027eb53174026
ThetaDev <thetadev@magenta.de>
2024-01-29 18:51:08 +0100

    fix(tests): dont ignore 404 error on rss feed

--- tests/youtube.rs
+++ tests/youtube.rs
@@ -1175,23 +1175,13 @@ mod channel_rss {
 
     #[rstest]
     fn get_channel_rss_empty(rp: RustyPipe) {
-        let channel = tokio_test::block_on(rp.query().channel_rss("UCAyFbMjB3qAQSZBj6NCuBSg"));
+        let channel =
+            tokio_test::block_on(rp.query().channel_rss("UCAyFbMjB3qAQSZBj6NCuBSg")).unwrap();
 
-        match channel {
-            Ok(channel) => {
-                assert_eq!(channel.id, "UCAyFbMjB3qAQSZBj6NCuBSg");
-                assert_eq!(channel.name, "Cheryl Calogero");
+        assert_eq!(channel.id, "UCAyFbMjB3qAQSZBj6NCuBSg");
+        assert_eq!(channel.name, "Cheryl Calogero");
 
-                assert!(channel.videos.is_empty());
-            }
-            Err(err) => {
-                assert!(
-                    matches!(err, Error::Extraction(ExtractionError::NotFound { .. })),
-                    "got: {}",
-                    err
-                );
-            }
-        }
+        assert!(channel.videos.is_empty());
     }
 
     #[rstest]
